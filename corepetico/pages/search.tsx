73c1cb445f9543b2226e0c9250a48e52b5e4d113
Kuba <01169177@pw.edu.pl>
2023-02-14 11:41:00 +0100

    search query

---
+++ corepetico/pages/search.tsx
@@ -0,0 +1,37 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+function Search() {
+    const router = useRouter();
+    const { location, startDate, endDate, noOfGuests } = router.query;
+
+
+
+  return (
+    <div className='h-screen'>
+        <Header />
+
+        <main className='flex'>
+            <section className='flex-grow pt-14 px-6'>
+                <p className='text-xs'>300+ Stays for 500 of Guests</p>
+
+                <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in {location}</h1>
+
+                <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
+                    <p className='button'>Cancelation Flexibility</p>
+                    <p className='button'>Type of Place</p>
+                    <p className='button'>Price</p>
+                    <p className='button'>Rooms and Beds</p>
+                    <p className='button'>More Filters</p>
+                </div>
+            </section>
+        </main>
+
+        <Footer />
+    </div>
+  )
+}
+
+export default Search
