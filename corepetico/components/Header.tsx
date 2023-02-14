e23d8da26176a73e1733e8b894eff3de89fc8447
Kuba <01169177@pw.edu.pl>
2023-02-14 13:53:59 +0100

    search nice looking

--- corepetico/components/Header.tsx
+++ corepetico/components/Header.tsx
@@ -12,7 +12,7 @@ import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
 import { useRouter } from 'next/router';
 
-function Header() {
+function Header({ placeholder }:any) {
   const [searchInput, setSearchInput] = useState('');
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
@@ -59,7 +59,13 @@ function Header() {
       </div>
 
       <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
-        <input value={searchInput} onChange={(e) => setSearchInput(e.target.value)} className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' type="text" placeholder='Start your search' />
+        <input 
+        value={searchInput} 
+        onChange={(e) => setSearchInput(e.target.value)} 
+        className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' 
+        type="text" 
+        placeholder={placeholder || 'Start your search' }
+        />
         <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
       </div>
 
--- corepetico/pages/search.tsx
+++ corepetico/pages/search.tsx
@@ -2,36 +2,63 @@ import { useRouter } from 'next/router'
 import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import { format } from 'date-fns';
+import InfoCard from '../components/InfoCard';
 
-function Search() {
+function Search({ searchResults }: any) {
     const router = useRouter();
     const { location, startDate, endDate, noOfGuests } = router.query;
 
+    const formattedStartDate = format(new Date(String(startDate)), "dd MMMM yy");
+    const formattedEndDate = format(new Date(String(endDate)), "dd MMMM yy");
+    const range = `${formattedStartDate} - ${formattedEndDate}`;
 
 
-  return (
-    <div className='h-screen'>
-        <Header />
+    return (
+        <div className='h-screen'>
+            <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`} />
 
-        <main className='flex'>
-            <section className='flex-grow pt-14 px-6'>
-                <p className='text-xs'>300+ Stays for 500 of Guests</p>
+            <main className='flex'>
+                <section className='flex-grow pt-14 px-6'>
+                    <p className='text-xs'>300+ Stays - {range} - for {noOfGuests} Guests</p>
 
-                <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in {location}</h1>
+                    <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in {location}</h1>
 
-                <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
-                    <p className='button'>Cancelation Flexibility</p>
-                    <p className='button'>Type of Place</p>
-                    <p className='button'>Price</p>
-                    <p className='button'>Rooms and Beds</p>
-                    <p className='button'>More Filters</p>
-                </div>
-            </section>
-        </main>
+                    <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
+                        <p className='button'>Cancelation Flexibility</p>
+                        <p className='button'>Type of Place</p>
+                        <p className='button'>Price</p>
+                        <p className='button'>Rooms and Beds</p>
+                        <p className='button'>More Filters</p>
+                    </div>
 
-        <Footer />
-    </div>
-  )
+                    <div className='flex flex-col'>
+                        {searchResults.map(({ id, url, title }: any) => (
+                            <InfoCard
+                                key={id}
+                                url={url}
+                                title={title}
+                            />
+                        ))}
+                    </div>
+
+                </section>
+            </main>
+
+            <Footer />
+        </div>
+    )
 }
 
 export default Search
+
+export async function getServerSideProps() {
+    const searchResults = await fetch('https://jsonplaceholder.typicode.com/albums/2/photos').then(res => res.json());
+
+    /* 'https://links.papareact.com/isz' */
+    return {
+        props: {
+            searchResults
+        }
+    }
+}
