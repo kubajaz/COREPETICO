46f16959b3e655c750af9f3eeb512eb6f46e8610
Kuba <01169177@pw.edu.pl>
2023-02-27 17:06:49 +0100

    fixed bug with dropdown not hidnig onClick

--- corepetico/components/Header/DropDown.tsx
+++ corepetico/components/Header/DropDown.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { MenuIcon, UserCircleIcon } from '@heroicons/react/solid'
 import { useAuth } from '../../context/AuthContext';
@@ -6,26 +6,28 @@ import { useAuth } from '../../context/AuthContext';
 
 function DropDown() {
     const { user, logout } = useAuth()
+    const [isVisible, setIsVisible] = useState(false)
 
-    return (
-        <div className="dropdown dropdown-bottom dropdown-end">
+    return <>
+        <div onClick={() => setIsVisible(!isVisible)} className="dropdown dropdown-bottom dropdown-end">
             <label tabIndex={0} className="btn m-1">
                 <div className='flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer text-gray-700'>
                     <MenuIcon className='h-6' />
                     <UserCircleIcon className='h-6' />
                 </div>
             </label>
-            <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                {user ? <li onClick={() => logout()}><Link style={{ textDecoration: 'none' }} href="/">Log Out</Link></li> :
-                    <>
-                        <li><Link style={{ textDecoration: 'none' }} href="/login">Logowanie</Link></li>
-                        <li><Link style={{ textDecoration: 'none' }} href="/signup">Rejestracja</Link></li>
-                    </>
-                }
+            {isVisible ?
+                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                    {user ? <li onClick={() => logout()}><Link style={{ textDecoration: 'none' }} href="/">Log Out</Link></li> :
+                        <>
+                            <li><Link style={{ textDecoration: 'none' }} href="/login">Logowanie</Link></li>
+                            <li><Link style={{ textDecoration: 'none' }} href="/signup">Rejestracja</Link></li>
+                        </>
+                    }
 
-            </ul>
+                </ul> : null}
         </div>
-    )
+    </>
 }
 
 export default DropDown
