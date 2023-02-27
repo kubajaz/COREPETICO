83500fca011a02c54b0e60addcc5dc5f948bb35b
Kuba <01169177@pw.edu.pl>
2023-02-27 09:00:18 +0100

    single modals

--- corepetico/components/Order/OrderModal.tsx
+++ corepetico/components/Order/OrderModal.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import ContentModal from "./ContentModal";
+import TimeModal from "./TimeModal";
 
 export default function Modal() {
-    const [showModal, setShowModal] = React.useState(false);
+    const [showModal, setShowModal] = React.useState(0); // 0-nothing, 1-place, 2-content
     return (
         <>
             <div className='absolute top-1/2 w-full text-center'>
@@ -9,62 +11,14 @@ export default function Modal() {
                 <button
                     className='text-blue-800 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'
                     type="button"
-                    onClick={() => setShowModal(true)}
+                    onClick={() => setShowModal(1)}
                 >
                     Zamów lekcję
                 </button>
             </div>
-            {showModal ? (
-                <>
-                    <div
-                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                    >
-                        <div className="relative w-auto my-6 mx-auto max-w-6xl">
-                            {/*content*/}
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                {/*header*/}
-                                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                    <h3 className="text-3xl font-semibold">
-                                        Zamów lekcję
-                                    </h3>
-                                    <button
-                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                            ×
-                                        </span>
-                                    </button>
-                                </div>
-                                {/*body*/}
-                                <div className="relative p-6 flex-auto">
-                                    <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                                    Lorem ipsum dolor sit amet. Et facilis quaerat ut eaque consequuntur eos quaerat expedita est itaque omnis. Qui nesciunt amet in soluta odit aut dolor distinctio. Aut excepturi beatae est ducimus aliquam qui accusantium perspiciatis nam suscipit laboriosam 33 ratione consequatur? 
-                                    </p>
-                                </div>
-                                {/*footer*/}
-                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                                    <button
-                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Zamknij
-                                    </button>
-                                    <button
-                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Zamów
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-                </>
-            ) : null}
+            {showModal === 1 ? (
+                <TimeModal setShowModal={setShowModal} />
+            ) : showModal === 2 ? <ContentModal setShowModal={setShowModal} /> : null}
         </>
     );
 }
